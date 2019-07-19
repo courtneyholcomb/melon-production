5890f385b8e087fbd575c60bbd2f6e8f24cef0b5
Courtney Holcomb <courtneyeholcomb@gmail.com>
2019-07-18 18:58:28 -0700

    Initial commit – created Squash class

---
+++ melons.py
@@ -0,0 +1,48 @@
+import robots
+
+
+class Melon(object):
+    """Melon."""
+
+    def __init__(self, melon_type):
+        """Initialize melon.
+
+        melon_type: type of melon being built.
+        """
+
+        self.melon_type = melon_type
+        self.weight = 0.0
+        self.color = None
+        self.stickers = []
+
+    def prep(self):
+        """Prepare the melon."""
+
+        robots.cleanerbot.clean(self)
+        robots.stickerbot.apply_logo(self)
+
+    def __str__(self):
+        """Print out information about melon."""
+
+        if self.weight <= 0:
+            return self.melon_type
+        else:
+            return "{} {:.2f} lbs {}".format(self.color,
+                                             self.weight,
+                                             self.melon_type)
+
+
+class Squash(Melon):
+    """Winter Squash, a Melon."""
+
+    def __init__(self):
+        """Create a Melon of Winter Squash type."""
+        self.super().__init__('Winter Squash')
+
+
+    def prep(self):
+        """Prep melon and paint green."""
+        self.super().prep()
+        robots.painterbot.paint(self)
+
+
